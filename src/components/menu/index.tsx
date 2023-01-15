fe69e505cc4eaba27ef710feae12ec050ab504d6
ChernyshevaNetology <chernyshevak87@mail.ru>
2023-01-15 14:16:49 +0200

    fix: fixed classes for active menu item

--- src/components/menu/index.tsx
+++ src/components/menu/index.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import cn from 'classnames';
 
 interface INavLinks {
   title: string;
@@ -10,22 +11,18 @@ interface IActiveClass {
   isActive: boolean;
 }
 
-const Menu = () => {
-  const linkClasses: string[] = ["menu__item", "menu__item-active"];
-  const navLinks: INavLinks[] = [
-    { title: "Главная", path: "/" },
-    { title: "Дрифт-такси", path: "/drift" },
-    { title: "Time Attack", path: "/timeattack" },
-    { title: "Forza Karting", path: "/forza" },
-  ];
-  const setActive = ({ isActive }: IActiveClass): string => {
-    return isActive ? linkClasses.join(" ") : linkClasses[0];
-  };
+const navLinks: INavLinks[] = [
+  { title: "Главная", path: "/" },
+  { title: "Дрифт-такси", path: "/drift" },
+  { title: "Time Attack", path: "/timeattack" },
+  { title: "Forza Karting", path: "/forza" },
+];
 
+const Menu = () => {
   return (
     <nav className="menu">
       {navLinks.map(({ title, path }) => (
-        <NavLink key={path} className={setActive} to={path}>
+        <NavLink key={path} to={path} className={'menu__item'} >
           {title}
         </NavLink>
       ))}
